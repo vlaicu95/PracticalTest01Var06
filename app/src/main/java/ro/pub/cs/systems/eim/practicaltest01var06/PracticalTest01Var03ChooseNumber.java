6f36554cd76631b3710ff71c12bbea2e1c7ae401
Student User <student@cti.pub.ro>
2018-04-02 12:27:52 +0300

    A done

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var03ChooseNumber.java
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.eim.practicaltest01var06;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticalTest01Var03ChooseNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var03_choose_number);
+
+
+    }
+}
